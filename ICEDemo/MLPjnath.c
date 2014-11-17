31f781f3c26e31c812ad78dcaa2eb19087766a59
marslin <marslin@gmail.com>
2014-11-17 16:30:27 +1000

    Add 'MLPjnath' file to integrate PJNATH.

---
+++ ICEDemo/MLPjnath.c
@@ -0,0 +1,9 @@
+//
+//  MLPjnath.c
+//  ICEDemo
+//
+//  Created by 林 政龍 on 2014/11/17.
+//  Copyright (c) 2014年 marstudio. All rights reserved.
+//
+
+#include "MLPjnath.h"
---
+++ ICEDemo/MLPjnath.h
@@ -0,0 +1,14 @@
+//
+//  MLPjnath.h
+//  ICEDemo
+//
+//  Created by 林 政龍 on 2014/11/17.
+//  Copyright (c) 2014年 marstudio. All rights reserved.
+//
+
+#ifndef __ICEDemo__MLPjnath__
+#define __ICEDemo__MLPjnath__
+
+#include <stdio.h>
+
+#endif /* defined(__ICEDemo__MLPjnath__) */
